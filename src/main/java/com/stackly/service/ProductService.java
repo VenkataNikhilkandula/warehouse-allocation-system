040242924abc305e77eaa93cc9bf4eb393590e1a
Venkata Nikhil Kandula <kvenkatanikhil333@gmail.com>
2026-05-04 12:56:45 +0530

    Update ProductService.java

--- src/main/java/com/stackly/service/ProductService.java
+++ src/main/java/com/stackly/service/ProductService.java
@@ -5,25 +5,9 @@ import com.stackly.DTO.ProductResponse;
 import com.stackly.entity.Product;
 
 import java.util.List;
-
-//public interface ProductService {
-
-//    ProductResponse create(ProductRequest request);
-//
-//    List<Product> getAll();
-//
-//    ProductResponse getById(Long id);
-//
-//    ProductResponse update(Long id, ProductRequest request);
-//
-//    void delete(Long id);
-//}
-
-
-
 public interface ProductService {
 
-    Product create(ProductRequest request);   // ✅ MUST MATCH
+    Product create(ProductRequest request);  
 
     List<Product> getAll();
 
